9c415fd9b240c68f346bdd4bb3c07b1e8d44e3f0
carlos richter <rrch.cr8@gmail.com>
2021-11-17 07:22:26 -0400

    completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,7 @@
+import { EmployeeService } from './employee.service';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -8,9 +11,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule, FormsModule
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
